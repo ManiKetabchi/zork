68b335a136e9ad2cac58a2946ba8c46159a48f1a
Mani Ketabchi <132390984+ManiKetabchi@users.noreply.github.com>
2024-06-09 23:33:04 +0000

    First Working Words

--- Weekend-At-Bernies-master/src/zork/GameInfo.java
+++ Weekend-At-Bernies-master/src/zork/GameInfo.java
@@ -2,6 +2,10 @@ package zork;
 
 public class GameInfo {
     public static String[] introMessage;
+    
+    public static String introMsg() {
+        return "ASIDIHUDFSAIUOADS";
+    }
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");  
