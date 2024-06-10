da34f5b7326fd964997128e433d00b86cd9d2f16
NoorA34 <nali@bayviewglen.ca>
2024-06-09 21:59:14 -0400

    done by eva

--- Weekend-At-Bernies-master/src/zork/CommandWords.java
+++ Weekend-At-Bernies-master/src/zork/CommandWords.java
@@ -1,37 +1,123 @@
 package zork;
-
+ 
+import java.util.ArrayList;
+ 
 public class CommandWords {
-  // a constant array that holds all valid command words
-  private static final String validCommands[] = { "go", "quit", "help", "eat", "look", "drop", "inventory", "examine", "use",
-  "open", "close", "attack"};
-
-  /**
-   * Constructor - initialise the command words.
-   */
-  public CommandWords() {
-    // nothing to do at the moment...
-  }
-
-  /**
-   * Check whether a given String is a valid command word. Return true if it is,
-   * false if it isn't.
-   **/
-  public boolean isCommand(String aString) {
-    for (String c : validCommands) {
-      if (c.equals(aString))
-        return true;
+    // A constant array that holds all valid command words
+    private static final String validCommands[] = {
+        "go", "quit", "help", "eat", "look",
+        "drop", "inventory", "examine", "use",
+        "open", "close", "attack"
+    };
+ 
+    private Inventory inventory;
+ 
+    /**
+     * Constructor - initialise the command words.
+     */
+    public CommandWords(Inventory inventory) {
+        this.inventory = inventory;
     }
-    // if we get here, the string was not found in the commands
-    return false;
-  }
-
-  /*
-   * Print all valid commands to System.out.
-   */
-  public void showAll() {
-    for (String c : validCommands) {
-      System.out.print(c + "  ");
+ 
+    /**
+     * Check whether a given String is a valid command word. Return true if it is,
+     * false if it isn't.
+     **/
+    public boolean isCommand(String aString) {
+        for (String c : validCommands) {
+            if (c.equals(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
     }
-    System.out.println();
-  }
+ 
+    /*
+     * Print all valid commands to System.out.
+     */
+    public void showAll() {
+        for (String c : validCommands) {
+            System.out.print(c + "  ");
+        }
+        System.out.println();
+    }
+ 
+    // Implementing commands
+ 
+    public void go(String direction, Player player) {
+        Room nextRoom = player.getCurrentRoom().nextRoom(direction);
+        if (nextRoom != null) {
+            player.setCurrentRoom(nextRoom);
+            System.out.println(player.getCurrentRoom().longDescription());
+        } else {
+            System.out.println("There is no door to " + direction + "!");
+        }
+    }
+ 
+    public void quit() {
+        System.out.println("Thank you for playing. Goodbye!");
+        System.exit(0);
+    }
+ 
+    public void help() {
+        System.out.println("You are lost. You are alone. You wander around the space station.");
+        System.out.println("Your command words are:");
+        showAll();
+    }
+ 
+    public void eat() {
+        System.out.println("You have eaten now and you are not hungry anymore.");
+    }
+ 
+    public void look(Player player) {
+        System.out.println(player.getCurrentRoom().longDescription());
+    }
+ 
+    public void drop(Item item, Player player) {
+        if (player.getInventory().removeItem(item) != null) {
+            player.getCurrentRoom().addItem(item);
+            System.out.println("You drop the " + item.getName() + " in the " + player.getCurrentRoom().getRoomName() + ".");
+        } else {
+            System.out.println("You don't have " + item.getName() + " in your inventory.");
+        }
+    }
+ 
+    public void inventory(Player player) {
+        System.out.println("You are carrying:");
+        for (Item item : player.getInventory().getItems()) {
+            System.out.println(item.getName() + ": " + item.getDescription());
+        }
+    }
+ 
+    public void examine(Item item) {
+        System.out.println("You examine the " + item.getName() + ": " + item.getDescription());
+    }
+ 
+    public void use(Item item) {
+        System.out.println("You use the " + item.getName() + ".");
+        // implement
+    }
+ 
+    public void open(Item item) {
+        if (item.isOpenable()) {
+            System.out.println("You open the " + item.getName() + ".");
+            item.open();
+        } else {
+            System.out.println("The " + item.getName() + " cannot be opened.");
+        }
+    }
+ 
+    public void close(Item item) {
+        if (item.isOpenable()) {
+            System.out.println("You close the " + item.getName() + ".");
+            // Implement 
+        } else {
+            System.out.println("The " + item.getName() + " cannot be closed.");
+        }
+    }
+ 
+    public void attack(Item item) {
+        System.out.println("You attack with the " + item.getName() + ".");
+        // Implement 
+    }
 }
--- Weekend-At-Bernies-master/src/zork/Parser.java
+++ Weekend-At-Bernies-master/src/zork/Parser.java
@@ -1,41 +1,99 @@
 package zork;
-
+ 
 import java.util.Scanner;
-
+ 
 public class Parser {
-  private CommandWords commands; // holds all valid command words
-  private Scanner in;
-
-  public Parser() {
-    commands = new CommandWords();
-    in = new Scanner(System.in);
-  }
-
-  public Command getCommand() throws java.io.IOException {
-    String inputLine = "";
-    String[] words;
-
-    System.out.print("> "); // print prompt
-
-    inputLine = in.nextLine();
-
-    words = inputLine.split(" ");
-
-    String word1 = words[0];
-    String word2 = null;
-    if (words.length > 1)
-      word2 = words[1];
-
-    if (commands.isCommand(word1))
-      return new Command(word1, word2);
-    else
-      return new Command(null, word2);
-  }
-
-  /**
-   * Print out a list of valid command words.
-   */
-  public void showCommands() {
-    commands.showAll();
-  }
+    private CommandWords commands; // holds all valid command words
+    private Scanner in;
+    private Inventory inventory;
+ 
+    public Parser() {
+        commands = new CommandWords();
+        in = new Scanner(System.in);
+        inventory = new Inventory(100); // Assuming maximum inventory weight is 100
+    }
+ 
+    public Command getCommand() throws java.io.IOException {
+        String inputLine = "";
+        String[] words;
+ 
+        System.out.print("> "); // print prompt
+ 
+        inputLine = in.nextLine();
+ 
+        words = inputLine.split(" ");
+ 
+        String word1 = words[0];
+        String word2 = null;
+        if (words.length > 1)
+            word2 = words[1];
+ 
+        if (commands.isCommand(word1)) {
+            if (word1.equals("go") && word2 != null) {
+                return new Command(word1, word2);
+            } else if (word1.equals("quit")) {
+                return new Command(word1, null);
+            } else if (word1.equals("help")) {
+                return new Command(word1, null);
+            } else if (word1.equals("eat") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.eat(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("look")) {
+                return new Command(word1, null);
+            } else if (word1.equals("drop") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.drop(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("inventory")) {
+                commands.inventory(inventory);
+            } else if (word1.equals("examine") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.examine(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("use") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.use(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("open") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.open(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("close") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.close(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else if (word1.equals("attack") && word2 != null) {
+                Item item = inventory.findItem(word2);
+                if (item != null) {
+                    commands.attack(item);
+                } else {
+                    System.out.println("You don't have " + word2 + " in your inventory.");
+                }
+            } else {
+                return new Command(word1, word2);
+            }
+        } else {
+            return new Command(null, word2);
+        }
+        return null; 
+    }
 }
